ffa02310b2164aef34fe79e257357d0978bcbbd4
HassanAfzal123 <43441286+HassanAfzal123@users.noreply.github.com>
2020-02-25 02:33:56 +0500

    new

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http=require('http');
 const app=require('./app');
 const server = http.createServer(app);
-server.listen(3000 , () => {
+server.listen(process.env.PORT || 5000 , () => {
     console.log(`server is up and running on port: ${3000}`);
 });

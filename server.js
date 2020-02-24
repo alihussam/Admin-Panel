8cdc8f55192ae0c443965eb9f88a9d924c096161
HassanAfzal123 <43441286+HassanAfzal123@users.noreply.github.com>
2020-02-25 02:07:52 +0500

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http=require('http');
 const app=require('./app');
 const server = http.createServer(app);
-server.listen(5000 , () => {
-    console.log(`server is up and running on port: ${5000}`);
+server.listen(3000 , () => {
+    console.log(`server is up and running on port: ${3000}`);
 });
